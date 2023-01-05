2e96a29e8869fd4fc44e04f39b4bb9b8ea69c6ef
mffonua <108596346+mffonua@users.noreply.github.com>
2023-01-04 17:51:56 -0700

    adding in world selection section, changing in world img

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("21c0bc0aba4507af")
+/******/ 		__webpack_require__.h = () => ("55ad4a9aa8371d86")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -10,11 +10,35 @@ import { ExploreCard, TitleText, TypingText  } from '../components';
 import { exploreWorlds } from '../constants';
 
 
-const Explore = () => (
+const Explore = () => {
+  const [active, setActive] = useState('world-2')
+  return (
   <section className={`${styles.paddings}`}
   id='explore'>
-    Explore section
+    <motion.div
+    variants={staggerContainer}
+    initial='hidden'
+    whileInView='show'
+    viewport={{once: false, amount:0.25}}
+    className={`${styles.innerWidth} mx-auto flex flex-col`}>
+      <TypingText title=' | The Worlds' textStyles='text-center' />
+      <TitleText  title={<>Choose any world you want 
+      <br className='md:block hidden'/> to explore</>}
+      textStyles='text-center'/>
+      <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'>
+        {exploreWorlds.map((world, index) => ( 
+          <ExploreCard 
+          key={world.id}
+          {...world}
+          index={index}
+          active={active}
+          handleClick={setActive}
+          />
+        ))}
+      </div>
+    </motion.div>
   </section>
-);
+)
+        };
 
 export default Explore;
