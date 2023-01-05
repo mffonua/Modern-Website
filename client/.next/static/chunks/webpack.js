071bb8486fab50c14f6417e0ae9b4471e5bbe3e6
mffonua <108596346+mffonua@users.noreply.github.com>
2023-01-04 21:28:37 -0700

    created meaningful readme

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "03a771ed252dbdb1"; }
+/******/ 		__webpack_require__.h = function() { return "0e37fdf1add968e7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
