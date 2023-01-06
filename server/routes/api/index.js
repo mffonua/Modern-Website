ce1ffb900f5371e545e3ab4e95ab344a339c03b4
Isaac Thorell <isaac.thorell@outlook.com>
2023-01-06 09:06:45 -0700

    seeding udpates

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const matchupRoutes = require('./matchup-routes');
-const techRoutes = require('./tech-routes.js');
+
 
 router.use('/matchup', matchupRoutes);
 router.use('/tech', techRoutes);
